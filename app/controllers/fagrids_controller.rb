5047fe098c04c34b800310c2c13bf4675ed5cf15
minato_saito <69403974+aigis1@users.noreply.github.com>
2020-12-04 03:14:46 +0900

    詳細画面

--- app/controllers/fagrids_controller.rb
+++ app/controllers/fagrids_controller.rb
@@ -1,2 +1,33 @@
 class FagridsController < ApplicationController
+
+  def index
+    @fagrids = Fagrid.order(id: "DESC")
+  end
+
+  def new
+    @fagrid = Fagrid.new
+  end
+
+  def create
+    @fagrid = Fagrid.new(fagrid_params)
+    binding.pry
+    if @fagrid.valid?
+      @fagrid.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @fagrid = Fagrid.find(params[:id])
+  end
+
+  private
+  
+  def fagrid_params
+    params.require(:fagrid).permit(:image, :quest_id, :attr_id, :job_id, :ex_ability_id, :ex_ability2_id, :limit_ability_id, :limit_ability2_id,
+      :member1, :member2, :member3, :member4, :member5,:main_summon_id, :seraphic_id, :atk_arculm_id, :hp_arculm_id, :difficulty_id, :time_about_id, :remark)
+  end
+
 end
