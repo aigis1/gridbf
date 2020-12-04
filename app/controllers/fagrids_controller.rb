75b8cd1d22a8935599c4840dc6f9fad2e3ccc9aa
minato_saito <69403974+aigis1@users.noreply.github.com>
2020-12-04 21:51:33 +0900

    検索機能beta

--- app/controllers/fagrids_controller.rb
+++ app/controllers/fagrids_controller.rb
@@ -10,7 +10,6 @@ class FagridsController < ApplicationController
 
   def create
     @fagrid = Fagrid.new(fagrid_params)
-    binding.pry
     if @fagrid.valid?
       @fagrid.save
       redirect_to root_path
@@ -23,11 +22,25 @@ class FagridsController < ApplicationController
     @fagrid = Fagrid.find(params[:id])
   end
 
+  def search
+    search_grid
+    @fagrids = Fagrid.order(id: "DESC")
+  end
+
+  def sort
+    search_grid
+    @result = @g.result    # 検索条件にマッチした商品の情報を取得
+  end
+
   private
   
   def fagrid_params
     params.require(:fagrid).permit(:image, :quest_id, :attr_id, :job_id, :ex_ability_id, :ex_ability2_id, :limit_ability_id, :limit_ability2_id,
       :member1, :member2, :member3, :member4, :member5,:main_summon_id, :seraphic_id, :atk_arculm_id, :hp_arculm_id, :difficulty_id, :time_about_id, :remark)
+  end
+
+  def search_grid
+    @g = Fagrid.ransack(params[:q]) # 検索オブジェクトを生成
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
   root to: 'fagrids#index'
+  get 'fagrids/search'
+  get 'fagrids/sort'
   resources :fagrids, only: [:index, :new, :create, :show]
+  
 end
